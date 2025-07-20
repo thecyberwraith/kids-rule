1e12b6ca10c0940d475bea22b0b746bc5bdb5130
Michael Lugo <michaelrlugo@gmail.com>
2025-07-20 13:39:02 -0400

    Closes #1
    More signals and variable clarity was added to the DamageHandler, which helped fix this bug. Now, the handler can signal when a revive occurs, at
    which time the RangerPlayer switches from the defeated state (which it
    *should* be in) to the idle state to resume play.

--- utility/DamageHandler.gd
+++ utility/DamageHandler.gd
@@ -1,18 +1,32 @@
+## Automatics traditional handling of health. It handles:
+## - Invincibility
+## - Checking if alive
+## - Common signals on types of health changes
 class_name DamageHandler extends Node
 
-enum State {ALIVE, DEAD}
 
 @export var health := CharacterResource.new("Health", 0.0, 10.0, 10.0)
 @export var invincible_duration: float = 1.5
-var state = State.ALIVE
+@export var invincible_when_revived: bool = true
 
-var invincible_time = 0.0
+# Keeps track if 
+var is_alive: bool:
+	get:
+		return true if health.value > 0 else false
+
+var is_invincible: bool:
+	get:
+		return _invincible_time > 0.0
+
+var _invincible_time = 0.0
 
 signal damaged(amount: float)
 signal healed(amount: float)
 
+signal invincibility_start
 signal invincibility_stop
 signal death
+signal revived
 
 
 func _ready():
@@ -22,7 +36,7 @@ func _ready():
 ## Takes a positive amount of damage and applies it to the health, assuming
 ## the player is not invincible.
 func damage(amount: float):
-	if invincible_time > 0 or amount < 0:
+	if is_invincible or amount < 0:
 		return
 	
 	health.value -= amount
@@ -36,24 +50,31 @@ func heal(amount: float):
 	health.value += amount
 
 
+func _start_invincibility():
+	_invincible_time = invincible_duration
+	invincibility_start.emit()
+
+
 func _handle_new_health_value(_res: CharacterResource, diff: float):
 	if diff < 0:
 		damaged.emit(-diff)
 		if health.value == 0:
 			death.emit()
 		else:
-			invincible_time = invincible_duration
+			_start_invincibility()
 	if diff > 0:
 		healed.emit(diff)
-	
-	state = State.ALIVE if health.value > 0 else State.DEAD
+		if diff == health.value:
+			revived.emit()
+			if invincible_when_revived:
+				_start_invincibility()
 
 
 func _process(dt):
-	if invincible_time == 0.0:
+	if not is_invincible:
 		return
 	
-	invincible_time -= dt
+	_invincible_time -= dt
 	
-	if invincible_time <= 0.0:
+	if not is_invincible:
 		invincibility_stop.emit()
