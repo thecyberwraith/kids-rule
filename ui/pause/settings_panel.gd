863def31679ca37226e219892e2a2ce55a0f51cf
Michael Lugo <michaelrlugo@gmail.com>
2025-07-24 08:27:07 -0400

    Added UI navigation to settings menu.

--- ui/pause/settings_panel.gd
+++ ui/pause/settings_panel.gd
@@ -9,12 +9,16 @@ var controls: Array[Control] = []
 
 func _ready():
 	_populate_settings()
-	focus_entered.connect(func(): controls[0].grab_focus())
+	focus_entered.connect(func(): 
+		controls[0].grab_focus()
+		for control in controls:
+			control.focus_neighbor_left = previous_focus.get_path()
+	)
 
 
 func _populate_settings():
 	var last_section: String = ""
-
+	
 	for setting in Settings.All:
 		if last_section != setting.section:
 			var new_sep: SettingsPanelItemSeparator = settings_sep.instantiate()
@@ -26,3 +30,11 @@ func _populate_settings():
 		container.add_child(new_control)
 		new_control.setting = setting
 		controls.append(new_control.control)
+		
+	for i in controls.size():
+		var before := wrapi(i-1, 0, controls.size())
+		var after := wrapi(i+1, 0, controls.size())
+
+		controls[i].focus_neighbor_bottom = controls[after].get_path()
+		controls[i].focus_neighbor_top = controls[before].get_path()
+	
